5003844ed042330e901f3635b97f5b5e8333bd02
GPrix <ggpalomeque@gmail.com>
2020-05-30 17:39:43 -0300

    Server shotdown now handled

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 	"time"
 
 	"github.com/GGP1/palo/pkg/http/rest"
@@ -28,7 +33,39 @@ func main() {
 		MaxHeaderBytes: 1 << 20,
 	}
 
-	// Run server
-	fmt.Println("Listening on port", server.Addr)
-	server.ListenAndServe()
+	serverErrors := make(chan error, 1)
+
+	// Start server listening for errors
+	go func() {
+		fmt.Println("Listening on port", server.Addr)
+		serverErrors <- server.ListenAndServe()
+	}()
+
+	shutdown := make(chan os.Signal, 1)
+	signal.Notify(shutdown, os.Interrupt, syscall.SIGTERM)
+
+	// Shutdown
+	select {
+	case err := <-serverErrors:
+		log.Fatalf("error: Listening and serving %s", err)
+
+	case <-shutdown:
+		log.Println("main: Start shutdown")
+
+		// Give outstanding requests a deadline for completion.
+		const timeout = 5 * time.Second
+		ctx, cancel := context.WithTimeout(context.Background(), timeout)
+		defer cancel()
+
+		// Asking listener to shutdown and load shed.
+		err := server.Shutdown(ctx)
+		if err != nil {
+			log.Printf("main : Graceful shutdown did not complete in %v : %v", timeout, err)
+			err = server.Close()
+		}
+
+		if err != nil {
+			log.Fatalf("main : Could not stop server gracefully : %v", err)
+		}
+	}
 }
