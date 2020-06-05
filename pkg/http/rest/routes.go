c6cd8a2129c65e6b564ef7b8842b758b8268e4a8
GPrix <ggpalomeque@gmail.com>
2020-06-05 19:14:19 -0300

    Added home page

--- pkg/http/rest/routes.go
+++ pkg/http/rest/routes.go
@@ -4,9 +4,10 @@ Package rest contains all the functions related to the rest api
 package rest
 
 import (
+	"io"
 	"net/http"
 
-	h "github.com/GGP1/palo/pkg/http/rest/handlers"
+	h "github.com/GGP1/palo/pkg/http/rest/handler"
 
 	"github.com/gorilla/mux"
 )
@@ -18,13 +19,6 @@ func SetupRouter() *mux.Router {
 	// Auth
 	r.HandleFunc("/login", h.Login()).Methods("POST")
 	r.HandleFunc("/logout", h.Logout()).Methods("GET")
-
-	// Users
-	r.HandleFunc("/users", h.GetUsers()).Methods("GET")
-	r.HandleFunc("/users/{id}", h.GetOneUser()).Methods("GET")
-	r.HandleFunc("/users/{id}", h.AddUser()).Methods("POST")
-	r.HandleFunc("/users/{id}", h.UpdateUser()).Methods("PUT")
-	r.HandleFunc("/users/{id}", h.DeleteUser()).Methods("DELETE")
 
 	// Products
 	r.HandleFunc("/products", h.GetProducts()).Methods("GET")
@@ -46,7 +40,25 @@ func SetupRouter() *mux.Router {
 	r.HandleFunc("/shops/{id}", h.UpdateShop()).Methods("PUT")
 	r.HandleFunc("/shops/{id}", h.DeleteShop()).Methods("DELETE")
 
+	// Users
+	r.HandleFunc("/users", h.GetUsers()).Methods("GET")
+	r.HandleFunc("/users/{id}", h.GetOneUser()).Methods("GET")
+	r.HandleFunc("/users/{id}", h.AddUser()).Methods("POST")
+	r.HandleFunc("/users/{id}", h.UpdateUser()).Methods("PUT")
+	r.HandleFunc("/users/{id}", h.DeleteUser()).Methods("DELETE")
+
+	// Home
+	r.HandleFunc("/", home()).Methods("GET")
+
 	http.Handle("/", r)
 
 	return r
+}
+
+func home() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "plain/text")
+		w.WriteHeader(http.StatusOK)
+		io.WriteString(w, "Welcome to the Palo home page")
+	}
 }
