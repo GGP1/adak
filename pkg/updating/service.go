575fd70b5187a0f38f5f3da9e72aae8a23aba88a
GPrix <ggpalomeque@gmail.com>
2020-06-05 19:14:37 -0300

    Updating service is now working fine

--- pkg/updating/service.go
+++ pkg/updating/service.go
@@ -4,31 +4,59 @@ Package updating includes database udpating operations
 package updating
 
 import (
-	"github.com/GGP1/palo/pkg/models"
-	stg "github.com/GGP1/palo/pkg/storage"
+	"github.com/GGP1/palo/pkg/model"
+	storage "github.com/GGP1/palo/pkg/storage"
 )
 
 // Service provides models updating operations.
 type Service interface {
-	UpdateUser(*models.User, string) error
-	UpdateProduct(*models.Product, string) error
-	UpdateShop(*models.Shop, string) error
+	UpdateUser(*model.User, string) error
+	UpdateProduct(*model.Product, string) error
+	UpdateShop(*model.Shop, string) error
 }
 
 // UpdateUser returns nil and updates a user
-func UpdateUser(user *models.User, id string) error {
-	stg.DB.Where("id=?", id).Update(user)
+func UpdateUser(user *model.User, id string) error {
+	err := storage.DB.Model(&user).Where("id=?", id).Update(
+		"firstname", user.Firstname,
+		"lastname", user.Lastname,
+		"email", user.Email).
+		Error
+
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
 
 // UpdateProduct returns nil and updates a product
-func UpdateProduct(product *models.Product, id string) error {
-	stg.DB.Where("id=?", id).Update(product)
+func UpdateProduct(product *model.Product, id string) error {
+	err := storage.DB.Model(&product).Where("id=?", id).Update(
+		"brand", product.Brand,
+		"category", product.Category,
+		"type", product.Type,
+		"description", product.Description,
+		"weight", product.Weight,
+		"price", product.Price).
+		Error
+
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
 
 // UpdateShop returns nil and updates a shop
-func UpdateShop(shop *models.Shop, id string) error {
-	stg.DB.Where("id=?", id).Update(shop)
+func UpdateShop(shop *model.Shop, id string) error {
+	err := storage.DB.Model(&shop).Where("id=?", id).Update(
+		"name", shop.Name).
+		Error
+
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
