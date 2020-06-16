af6c2ae69f308e648ff60471eb1fe219595f3a8e
GPrix <ggpalomeque@gmail.com>
2020-06-16 18:20:35 -0300

    Database connection test

--- pkg/storage/storage_test.go
+++ pkg/storage/storage_test.go
@@ -1,4 +1,4 @@
-package main
+package storage_test
 
 import (
 	"testing"
@@ -8,23 +8,24 @@ import (
 	_ "github.com/lib/pq"
 )
 
+// Checkboxes
 const (
 	succeed = "\u2713"
 	failed  = "\u2717"
 )
 
-func TestAPI(t *testing.T) {
+func TestDatabase(t *testing.T) {
 	t.Run("database", database)
 }
 
 func database(t *testing.T) {
 	t.Log("Given the need to test database connection.")
 	{
-		t.Logf("\tTest 0:\tWhen checking the database connection")
+		t.Logf("\tTest 0:\tWhen checking the database connection.")
 		{
 			db, err := storage.Connect()
 			if err != nil {
-				t.Fatalf("\t%s\tShould be able to connect to the database : %v", failed, err)
+				t.Fatalf("\t%s\tShould be able to connect to the database: %v", failed, err)
 			}
 			t.Logf("\t%s\tShould be able to connect to the database.", succeed)
 
