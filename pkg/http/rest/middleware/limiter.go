0322a6e3cce99feefbba7aef4135809c85085a3c
GPrix <ggpalomeque@gmail.com>
2020-06-20 18:46:25 -0300

    Improve rate limiter

--- pkg/http/rest/middleware/limiter.go
+++ pkg/http/rest/middleware/limiter.go
@@ -13,16 +13,18 @@ import (
 	"golang.org/x/time/rate"
 )
 
-// visitor holds the rate limiter for each visitor
-// and the last time the visitor was seen
+// visitor holds the rate limiter for each visitor and the
+// last time the visitor was seen
 type visitor struct {
-	limiter   *rate.Limiter
-	lastVisit time.Time
+	// Control how frequent the events are allowed to happen
+	limiter *rate.Limiter
+	// The latest time a visitor have been seen
+	lastSeen time.Time
 }
 
 // visitors map holds the values of the visitors
 var visitors = make(map[string]*visitor)
-var mutex sync.Mutex
+var mutex sync.RWMutex
 
 func init() {
 	go cleanupVisitors()
@@ -39,7 +41,8 @@ func LimitRate(next http.Handler) http.Handler {
 			return
 		}
 
-		limiter := getVisitor(ip)
+		limiter := getVisitor(ip, 1, 3)
+		// Control how frequent events may happen
 		if limiter.Allow() == false {
 			http.Error(w, http.StatusText(429), http.StatusTooManyRequests)
 			return
@@ -51,36 +54,37 @@ func LimitRate(next http.Handler) http.Handler {
 
 // Checks if the visitors map exists and creates a new one
 // if not, update last visit and return the visitor limiter
-func getVisitor(ip string) *rate.Limiter {
-	mutex.Lock()
-	defer mutex.Unlock()
+func getVisitor(ip string, r rate.Limit, b int) *rate.Limiter {
+	mutex.RLock()
+	defer mutex.RUnlock()
 
 	v, exists := visitors[ip]
 	if !exists {
-		// Implement a 3 tokens bucket, initially full
-		// and refilled at a rate of 1 token per second
-		limiter := rate.NewLimiter(1, 3)
+		// Implement a "token bucket" of size b, initially
+		// full and refilled at a rate of r token per second
+		limiter := rate.NewLimiter(r, b)
 		// Save visitor
 		visitors[ip] = &visitor{limiter, time.Now()}
 		return limiter
 	}
 
-	// Update visitor last visit
-	v.lastVisit = time.Now()
+	// Update visitor last event
+	v.lastSeen = time.Now()
 	return v.limiter
 }
 
-// Every minute check the visitors in the map that
-// haven't been seen for 15 minutes
+// Every minute look for the visitors in the map that
+// haven't been seen for 10 minutes
 func cleanupVisitors() {
 	for {
 		time.Sleep(time.Minute)
 
-		mutex.Lock()
+		mutex.RLock()
 		for ip, v := range visitors {
-			if time.Since(v.lastVisit) > 15*time.Minute {
+			if time.Since(v.lastSeen) > 10*time.Minute {
 				delete(visitors, ip)
 			}
 		}
+		mutex.RUnlock()
 	}
 }
