9a126ba20891f39b3188713f0b93aa128a5fc5d7
GPrix <ggpalomeque@gmail.com>
2020-08-01 01:05:00 -0300

    Add terminal colors to logger

--- pkg/http/rest/middleware/logger.go
+++ pkg/http/rest/middleware/logger.go
@@ -8,6 +8,17 @@ import (
 	"time"
 )
 
+const (
+	green  = "\033[97;42m"
+	white  = "\033[90;47m"
+	yellow = "\033[90;43m"
+	red    = "\033[97;41m"
+	blue   = "\033[97;44m"
+	purple = "\033[97;45m"
+	cyan   = "\033[97;46m"
+	reset  = "\033[0m"
+)
+
 // LoggingResponseWriter helps us intercept the response status code.
 type loggingResponseWriter struct {
 	http.ResponseWriter
@@ -46,13 +57,57 @@ func LogFormatter(next http.Handler) http.Handler {
 		path := r.URL.Path
 		latency := time.Since(start)
 
-		log := fmt.Sprintf("[PALO] %v | %3d | %-10v | %-7s | %#v",
+		statusColor := statusCodeColor(status)
+		methodColor := methodColor(method)
+		resetColor := resetColor()
+
+		log := fmt.Sprintf("[PALO] %v | %s %3d %s | %-10v | %s %-7s %s | %#v",
 			timestamp,
-			status,
+			statusColor, status, resetColor,
 			latency,
-			method,
+			methodColor, method, resetColor,
 			path)
 
 		fmt.Println(log)
 	})
+}
+
+func statusCodeColor(code int) string {
+	switch {
+	case code >= http.StatusOK && code < http.StatusMultipleChoices:
+		return green
+	case code >= http.StatusMultipleChoices && code < http.StatusBadRequest:
+		return white
+	case code >= http.StatusBadRequest && code < http.StatusInternalServerError:
+		return yellow
+	default:
+		return red
+	}
+}
+
+func methodColor(reqMethod string) string {
+	method := reqMethod
+
+	switch method {
+	case http.MethodGet:
+		return green
+	case http.MethodPost:
+		return blue
+	case http.MethodPut:
+		return cyan
+	case http.MethodDelete:
+		return red
+	case http.MethodPatch:
+		return yellow
+	case http.MethodHead:
+		return purple
+	case http.MethodOptions:
+		return white
+	default:
+		return reset
+	}
+}
+
+func resetColor() string {
+	return reset
 }
