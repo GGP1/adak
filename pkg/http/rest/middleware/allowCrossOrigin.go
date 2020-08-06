a3bd0eee861607ee73ddff6cf35c026e7a961979
GPrix <ggpalomeque@gmail.com>
2020-08-06 17:35:03 -0300

    Change web client port

--- pkg/http/rest/middleware/allowCrossOrigin.go
+++ pkg/http/rest/middleware/allowCrossOrigin.go
@@ -8,7 +8,7 @@ import (
 func AllowCrossOrigin(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		// Allow localhost on port 3000 to send or receive data from our server
-		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
+		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:4001")
 
 		next.ServeHTTP(w, r)
 	})
